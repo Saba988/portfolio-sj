609ac81091fa429f069989d83e08fc65e1cefe4c
Saba Junaid <sabajunaid146@gmail.com>
2024-11-10 23:24:24 +0500

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,14 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+// next.config.js
+module.exports = {
+  images: {
+    domains: ['dummyimage.com'],
+  },
+};
+
+
+export default nextConfig;
